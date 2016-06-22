74b64ce791d57ea75c8dad12a244e630eddd0d3d
Mark Johnson <markjohnson303@gmail.com>
2016-06-22 15:42:39 -0600

    added earthquake list

--- scripts/app.js
+++ scripts/app.js
@@ -7,6 +7,10 @@ var template;
 $(document).on("ready", function() {
 	$.get(weekly_quakes_endpoint, function(data){
 		console.log(data);
+		var source = $('#quakes-list').html();
+		var template = Handlebars.compile(source);
+		var hmm = template({quake: data.features});
+		$("#info").append(hmm);
 	});
 
 });
